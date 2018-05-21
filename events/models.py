efe32d03aeee0975396db7c0c02f843262fc4fb1
Alfredo <gonzalezalfredo101@gmail.com>
2018-05-21 15:49:24 -0700

    added events models and views. some fixes needed

--- events/models.py
+++ events/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    approved = models.BooleanField(blank=True)
+    approved_at = models.DateTimeField(blank=True, null=True)
+
+    image = models.ImageField(width_field=500, height_field=500, blank=True)
+    description = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    more_details_link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
--- events/views.py
+++ events/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from events.models import Event
 # from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
     # dictionary containing dynamic page content
+    event_list = Event.objects.order_by('created_at')[:10]
     context_dict = {
-        'heading': 'Upcoming Events'
+        'heading': 'Upcoming Events',
+        'events': event_list
     }
     return render(request, 'events/index.html', context=context_dict)
