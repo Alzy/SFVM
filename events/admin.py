3509042ca9fbc1b0fa4d683283f9fe22ccc91e23
Alfredo <gonzalezalfredo101@gmail.com>
2018-05-24 13:59:58 -0700

    added approved and created_at columns to events model admin page

--- events/admin.py
+++ events/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from events.models import Event
 
 
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'approved', 'created_at',)
+
+
 # Register your models here.
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
